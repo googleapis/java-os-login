913c8c755d924f55ad4e96d210a8dcf3d3dd5071
yoshi-automation <yoshi-automation@google.com>
2020-12-10 15:32:05 -0800

    fix: Update gapic-generator-java to 0.0.7
    Committer: @miraleung
    PiperOrigin-RevId: 345476969
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Dec 3 10:07:32 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 7be2c821dd88109038c55c89f7dd48f092eeab9d
    Source-Link: https://github.com/googleapis/googleapis/commit/7be2c821dd88109038c55c89f7dd48f092eeab9d

--- proto-google-cloud-os-login-v1/src/main/java/com/google/cloud/oslogin/common/PosixAccountName.java
+++ proto-google-cloud-os-login-v1/src/main/java/com/google/cloud/oslogin/common/PosixAccountName.java
@@ -91,7 +91,7 @@ public class PosixAccountName implements ResourceName {
   public static List<String> toStringList(List<PosixAccountName> values) {
     List<String> list = new ArrayList<>(values.size());
     for (PosixAccountName value : values) {
-      if (Objects.isNull(value)) {
+      if (value == null) {
         list.add("");
       } else {
         list.add(value.toString());
@@ -106,14 +106,14 @@ public class PosixAccountName implements ResourceName {
 
   @Override
   public Map<String, String> getFieldValuesMap() {
-    if (Objects.isNull(fieldValuesMap)) {
+    if (fieldValuesMap == null) {
       synchronized (this) {
-        if (Objects.isNull(fieldValuesMap)) {
+        if (fieldValuesMap == null) {
           ImmutableMap.Builder<String, String> fieldMapBuilder = ImmutableMap.builder();
-          if (!Objects.isNull(user)) {
+          if (user != null) {
             fieldMapBuilder.put("user", user);
           }
-          if (!Objects.isNull(project)) {
+          if (project != null) {
             fieldMapBuilder.put("project", project);
           }
           fieldValuesMap = fieldMapBuilder.build();
